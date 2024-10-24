572f23c667499301be78797c7c4680d33c11387e
KiseRyota03 <tthiep.cv@gmail.com>
2024-10-24 23:07:37 +0700

    fix(swagger): update dto

--- src/modules/book/dto/create-book.dto.ts
+++ src/modules/book/dto/create-book.dto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsOptional, IsArray } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 import { Types } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateBookDto {
   @ApiProperty({
@@ -10,81 +10,34 @@ export class CreateBookDto {
   @IsNotEmpty()
   title: string;
 
-  @ApiProperty({
-    description: 'A brief plot summary of the book',
-    example: 'A story about the American dream set in the 1920s',
-  })
-  @IsOptional() // This should be required based on the schema
+  @IsOptional()
   plot: string;
 
-  @ApiProperty({
-    description: 'URL of the book cover image',
-    example: 'https://i.postimg.cc/8ckPPDky/image-1.png',
-    required: false,
-  })
   @IsOptional()
   coverImage?: string;
 
-  @ApiProperty({
-    description: 'URL of the author image',
-    example: 'https://i.postimg.cc/jq1v1hhR/image.png',
-    required: false,
-  })
   @IsOptional()
   authorImage?: string;
 
-  @ApiProperty({
-    description: 'The ID of the author',
-    example: '6701646ca74cc28551f85cb8',
-  })
   @IsOptional()
   authorId: string;
 
-  @ApiProperty({
-    description: 'The name of the author',
-    example: 'SOW',
-  })
   @IsOptional()
   authorName: string;
 
-  @ApiProperty({
-    description: 'Array of Genre IDs',
-    example: ['670fe41b16a89971003bea08', '670fe42b16a89971003bea0a'], // Example ObjectIds
-    required: false,
-  })
   @IsOptional()
   @IsArray()
   tags?: Types.ObjectId[];
 
-  @ApiProperty({
-    description: 'Total number of views',
-    example: 300,
-    required: false,
-  })
   @IsOptional()
   views?: number;
 
-  @ApiProperty({
-    description: 'Total number of votes',
-    example: 100,
-    required: false,
-  })
   @IsOptional()
   totalVotes?: number;
 
-  @ApiProperty({
-    description: 'Total number of positive votes',
-    example: 80,
-    required: false,
-  })
   @IsOptional()
   positiveVotes?: number;
 
-  @ApiProperty({
-    description: 'Array of Chapter IDs',
-    example: [], // Example ObjectIds
-    required: false,
-  })
   @IsOptional()
   @IsArray()
   chapters?: Types.ObjectId[];
--- src/modules/chapter/dto/create-chapter.dto.ts
+++ src/modules/chapter/dto/create-chapter.dto.ts
@@ -2,34 +2,16 @@ import { IsNotEmpty, IsOptional, IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateChapterDto {
-  @ApiProperty({
-    description: 'The title of the chapter',
-    example: 'Introduction to Programming', // Example value for the title
-  })
   @IsOptional()
   title: string;
 
-  @ApiProperty({
-    description: 'The chapter number (optional)',
-    example: 1, // Example value for chapterNumber
-    required: false, // Not required since it's optional
-  })
   @IsOptional()
   @IsNumber() // Adding a validation rule to ensure this is a number
   chapterNumber?: number;
 
-  @ApiProperty({
-    description: 'URL of the chapter image (optional)',
-    example: 'https://example.com/chapter-image.jpg', // Example value for chapterImage
-    required: false,
-  })
   @IsOptional()
   chapterImage?: string;
 
-  @ApiProperty({
-    description: 'The content of the chapter',
-    example: 'This chapter covers the basics of programming...', // Example value for the content
-  })
   @IsOptional()
   content: string;
 }
